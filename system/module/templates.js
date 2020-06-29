5ea7c43357571ccf96291e18ab60400b6951e8e4
Patrick Bauer <patrick.bauer@mayflower.de>
2020-06-30 01:20:25 +0200

    More layout experiments

--- system/module/templates.js
+++ system/module/templates.js
@@ -6,6 +6,9 @@ export const preloadHandlebarsTemplates = async function () {
     const templatePaths = [
         'systems/fatex/templates/actors/parts/information.html',
         'systems/fatex/templates/actors/parts/artwork.html',
+        'systems/fatex/templates/actors/parts/header.html',
+        'systems/fatex/templates/actors/parts/footer.html',
+        'systems/fatex/templates/actors/parts/skills.html',
     ];
 
     return loadTemplates(templatePaths);
