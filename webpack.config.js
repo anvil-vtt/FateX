30ee1845d8336ef8d924f766936ebe5ee93c87d9
Patrick Bauer <patrick.bauer@mayflower.de>
2020-08-02 15:46:46 +0200

    Build into dist folder

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,26 @@
 const path = require('path');
+const CopyPlugin = require('copy-webpack-plugin');
 
-module.exports = env => {
+
+module.exports = environment => {
+    environment = environment || {};
+    environment.watch = environment.watch || false;
+    environment.mode = environment.mode || 'development';
+
     return {
         entry: './src/fatex.js',
-        mode: 'development',
-        watch: env === undefined ? false : env.watch,
+        mode: environment.mode,
+        watch: environment.watch,
         output: {
             filename: 'fatex.js',
-            path: path.resolve(__dirname, 'system'),
-        }
+            path: path.resolve(__dirname, 'dist'),
+        },
+        plugins: [
+            new CopyPlugin({
+                patterns: [
+                    { from: 'system' },
+                ],
+            }),
+        ],
     }
 };
