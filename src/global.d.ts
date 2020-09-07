2be01dca4662e90e46fb6f85f4843dbff9f7c861
Patrick Bauer <patrick.jean.bauer@googlemail.com>
2020-09-07 18:10:58 +0200

    Updated dependencies and removed consecutive build errors

--- src/global.d.ts
+++ src/global.d.ts
@@ -1,7 +1,6 @@
 import { FatexConfig } from "./config.ts";
 
 declare global {
-    const __ALL_TEMPLATES__: string;
     const CONFIG: {
         FateX: FatexConfig;
     };
--- src/module/helper/TemplatePreloader.ts
+++ src/module/helper/TemplatePreloader.ts
@@ -5,6 +5,7 @@ export class TemplatePreloader {
      * Preload a set of templates to compile and cache them for fast access during rendering
      */
     static async preloadHandlebarsTemplates() {
+        // @ts-ignore
         const templatePaths = [__ALL_TEMPLATES__];
 
         return loadTemplates(templatePaths);
