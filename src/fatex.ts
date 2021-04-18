864ed46870399382030c36f0c00e8ecaec7012f6
Patrick Bauer <patrick.jean.bauer@googlemail.com>
2021-04-18 21:15:41 +0200

    Cleaup

--- src/fatex.ts
+++ src/fatex.ts
@@ -114,24 +114,22 @@ ActorGroupFeature.hooks();
 /* -------------------------------- */
 /*	Webpack HMR                     */
 /* -------------------------------- */
-if (process.env.NODE_ENV === "development") {
-    if (module.hot) {
-        module.hot.accept();
+if (module.hot) {
+    module.hot.accept();
 
-        if (module.hot.status() === "apply") {
-            for (const template in _templateCache) {
-                if (Object.prototype.hasOwnProperty.call(_templateCache, template)) {
-                    delete _templateCache[template];
+    if (module.hot.status() === "apply") {
+        for (const template in _templateCache) {
+            if (Object.prototype.hasOwnProperty.call(_templateCache, template)) {
+                delete _templateCache[template];
+            }
+        }
+
+        TemplatePreloader.preloadHandlebarsTemplates().then(() => {
+            for (const application in ui.windows) {
+                if (Object.prototype.hasOwnProperty.call(ui.windows, application)) {
+                    ui.windows[application].render(true);
                 }
             }
-
-            TemplatePreloader.preloadHandlebarsTemplates().then(() => {
-                for (const application in ui.windows) {
-                    if (Object.prototype.hasOwnProperty.call(ui.windows, application)) {
-                        ui.windows[application].render(true);
-                    }
-                }
-            });
-        }
+        });
     }
 }
