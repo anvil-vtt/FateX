849e026e238b71560326a006a585914ef9668ef5
Phil <me@phillip.best>
2021-07-14 03:36:35 +0100

    Adds a basic implementation of the getData method in the ThemeConfig class. Temporarily disables type checking on the ThemeConfiguration file. Updates the theme-config template to use initial values provided by the ThemeConfig class. Adds currently non-functional buttons for saving a theme, reverting to the default, and closing without making changes.

--- src/module/components/Configuration/ThemeConfiguration.ts
+++ src/module/components/Configuration/ThemeConfiguration.ts
@@ -1,3 +1,5 @@
+// @ts-nocheck
+
 export class ThemeConfig extends FormApplication {
     static get defaultOptions() {
         return mergeObject(super.defaultOptions, {
@@ -10,8 +12,15 @@ export class ThemeConfig extends FormApplication {
         } as BaseEntitySheet.Options)
     }
 
-    // @ts-ignore
     async _updateObject(event, formData) {
         return;
+    }
+
+    getData(options?: Application.RenderOptions): FormApplication.Data<Record<string, unknown>, FormApplication.Options> {
+        return mergeObject(super.getData(options), CONFIG.FateX.global.defaultStyles);
+    }
+
+    async close(options?: FormApplication.CloseOptions): Promise<void> {
+        await super.close(options);
     }
 }
